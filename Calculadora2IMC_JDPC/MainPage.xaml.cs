032954c4a58c0308aa280667e26687531e6ffcc9
PC <danyel.16@gmail.com>
2023-12-04 03:19:03 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora2IMC_JDPC/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora2IMC_JDPC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //Declarar variable y asignarle valor
+            var altura = float.Parse(Altura.Text);
+            var peso = float.Parse(Peso.Text);
+
+            //Realizar el calculo
+            var imc = peso/(altura*altura);
+
+
+            //enviar resultado
+            IMC.Text = imc.ToString();
+
+            string resultado = "";
+            if (imc < 18.5)
+
+            {
+                resultado = "Bajo peso";
+            }
+            else if (imc >= 18.5 && imc< 24.4)
+
+            {
+                resultado = "Peso normal";
+            }
+            else if (imc > 24.9 && imc<= 29.9)
+
+            {
+                resultado = "Sobrepeso";
+            }
+            else if (imc>29.9)
+            {
+                resultado = "Tienes obesidad, deja la coca";
+            }
+
+            DisplayAlert("Resultado ", resultado, "Quitar");
+        }
+    }
+}
---
+++ UITest1/Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+namespace UITest1
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            //Se ejecutara antes de empezar cualquier prueba de UI
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            app.Screenshot("Welcome screen.");
+
+            Assert.IsTrue(results.Any());
+        }
+
+
+        [Test]
+        public void Agregar60_170UITest()
+        {
+
+
+            app.EnterText("EntadaA","60");
+            app.EnterText("EntadaB", "170");
+
+
+            app.Tap("Entradas");
+
+           var appResultado= app.Query("Resultado").First(result => result.Text == "12");
+            Assert.IsTrue(appResultado !=null, "No muestra el resultado esperado");
+        }
+
+    }
+}
